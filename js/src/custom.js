7433db6bb2a370374d9b4f353aee6995429b528c
Travis CI User <travis@example.org>
2020-02-08 19:53:12 +0800

    Site updated: 2020-02-08 19:53:12

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,7 +1,18 @@
+$(window).resize(function() {
+    if ($('.site-nav-toggle').css('display') !== 'none') {
+        $("#background-img").remove();
+    } else {
+        $("#background-img").remove();
+        $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
+    }
+})
 $("#main").lazyload({
     effect: "fadeIn"
 });
 $("document").ready(
     function() {
-        $("head").append(`<style>.main::before {background-image: url('/images/background.webp');}</style>`);
+        if ($('.site-nav-toggle').css('display') === 'none') {
+            $("#background-img").remove();
+            $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
+        }
     })
