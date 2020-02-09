363157a7762b95b4a963bdaffcd21e72bef2666e
Travis CI User <travis@example.org>
2020-02-09 11:40:47 +0800

    Site updated: 2020-02-09 11:40:47

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,18 +1,1 @@
-$(window).resize(function() {
-    if ($('.site-nav-toggle').css('display') !== 'none') {
-        $("#background-img").remove();
-    } else {
-        $("#background-img").remove();
-        $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
-    }
-})
-$("#main").lazyload({
-    effect: "fadeIn"
-});
-$("document").ready(
-    function() {
-        if ($('.site-nav-toggle').css('display') === 'none') {
-            $("#background-img").remove();
-            $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
-        }
-    })
+$(window).resize(function(){"none"!==$(".site-nav-toggle").css("display")?$("#background-img").remove():($("#background-img").remove(),$("head").append("<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>"))}),$("#main").lazyload({effect:"fadeIn"}),$("document").ready(function(){"none"===$(".site-nav-toggle").css("display")&&($("#background-img").remove(),$("head").append("<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>"))});
