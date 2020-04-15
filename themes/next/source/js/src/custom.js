c42ff804044a7b1deb10b0c087be611540918d06
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-04-15 22:53:04 +0800

    update

--- themes/next/source/js/src/custom.js
+++ themes/next/source/js/src/custom.js
@@ -1,18 +1,18 @@
-$(window).resize(function() {
-    if ($('.site-nav-toggle').css('display') !== 'none') {
-        $("#background-img").remove();
-    } else {
-        $("#background-img").remove();
-        $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
-    }
-})
+// $(window).resize(function() {
+//     if ($('.site-nav-toggle').css('display') !== 'none') {
+//         $("#background-img").remove();
+//     } else {
+//         $("#background-img").remove();
+//         $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
+//     }
+// })
 $("#main").lazyload({
     effect: "fadeIn"
 });
-$("document").ready(
-    function() {
-        if ($('.site-nav-toggle').css('display') === 'none') {
-            $("#background-img").remove();
-            $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
-        }
-    })
+// $("document").ready(
+//     function() {
+//         if ($('.site-nav-toggle').css('display') === 'none') {
+//             $("#background-img").remove();
+//             $("head").append(`<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>`);
+//         }
+//     })
