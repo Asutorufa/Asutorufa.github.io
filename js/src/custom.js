7b23841f8031b977efee6ec8fe02d5eb84b25413
Travis CI User <travis@example.org>
2020-04-15 22:56:32 +0800

    Site updated: 2020-04-15 22:56:32

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,1 +1,1 @@
-$(window).resize(function(){"none"!==$(".site-nav-toggle").css("display")?$("#background-img").remove():($("#background-img").remove(),$("head").append("<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>"))}),$("#main").lazyload({effect:"fadeIn"}),$("document").ready(function(){"none"===$(".site-nav-toggle").css("display")&&($("#background-img").remove(),$("head").append("<style id='background-img'>.main::before {background-image: url('/images/background.webp');}</style>"))});
+$("#main").lazyload({effect:"fadeIn"});
